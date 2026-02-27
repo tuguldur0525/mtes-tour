bc8f0d90eaf45f261f8f87e0142059327dc2106f
tuguldur0525 <tokut243@gmail.com>
2026-02-27 18:28:24 +0800

    speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin", "latin-ext"],
